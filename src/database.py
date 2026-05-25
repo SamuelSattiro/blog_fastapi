39e98b049a06d53bc0a185bf191cd592a140e0a4
SamucaSupertramp <samucasupertramp@gmail.com>
2026-05-25 19:26:13 -0300

    configurando o db3

--- src/database.py
+++ src/database.py
@@ -3,12 +3,10 @@ import sqlalchemy as sa
 import os
 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sa.MetaData()
 
-engine = sa.create_engine(
-    DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
-)
+engine = sa.create_engine(DATABASE_URL)
