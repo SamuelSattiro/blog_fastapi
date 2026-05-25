0b6915f7ac4a6adefb0c580c25e8685d46d690e8
SamucaSupertramp <samucasupertramp@gmail.com>
2026-05-25 19:22:28 -0300

    configurando o db2

--- src/database.py
+++ src/database.py
@@ -9,7 +9,6 @@ database = databases.Database(DATABASE_URL)
 
 metadata = sa.MetaData()
 
-if os.getenv("RENDER"):
-    engine = sa.create_engine(DATABASE_URL)
-else:
-    engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = sa.create_engine(
+    DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+)
